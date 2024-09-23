bdf5c0e56f5a4253996d495f6484932b63d9f871
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2024-09-23 21:28:16 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,3 +1,12 @@
+function editNav() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+}
+
 // Function to open the modal
 function openModal() {
   resetErrorMessages();
